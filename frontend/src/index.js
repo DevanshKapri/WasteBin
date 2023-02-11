af26ca8c13944306f7926cbced5a123e32209b86
shubh asati <shubhtrikal9876@gmail.com>
2023-02-11 07:22:25 +0530

    feat : dashboard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
+import Dashboard from "./pages/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,6 +14,7 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path = "/dashboard" element = {<Dashboard />} />
       </Routes>
     </BrowserRouter>
     {/* <App /> */}
