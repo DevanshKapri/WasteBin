7a0a1cf90c1d826e219aaf6e0e68c31b422dc6c5
paragjjw <pjjw30@gmail.com>
2023-02-11 06:24:30 +0530

    auth pages added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,6 +11,8 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
     {/* <App /> */}
