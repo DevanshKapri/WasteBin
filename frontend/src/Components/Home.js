cabeead116f028c28acbbe6696a31f013a188306
paragjjw <pjjw30@gmail.com>
2023-02-11 17:09:41 +0530

    minor changes

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import BannerBackground from "../Assets/home-banner-background.png";
-import BannerImage from "../Assets/home-banner-image.png";
+import BannerImage from "../Assets/cover-image.jpg";
 import Navbar from "./Navbar";
 import { FiArrowRight } from "react-icons/fi";
 
@@ -25,7 +25,11 @@ const Home = () => {
           </button>
         </div>
         <div className="home-image-section">
-          <img src={BannerImage} alt="" />
+          <img
+            src={BannerImage}
+            style={{ borderRadius: "115px", width: "250px" }}
+            alt=""
+          />
         </div>
       </div>
     </div>
--- frontend/src/pages/Dashboard/UserDistance.js
+++ frontend/src/pages/Dashboard/UserDistance.js
@@ -1,126 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CollectorResponse() {
+  const [userDist, setUserDist] = useState([
+    {
+      name: "abcd",
+      date: "2000-10-06T22:22:55",
+      waste_type: "Recyclable",
+      msg: "jregkjfchx4kjgn",
+      distance: 65.48,
+    },
+    {
+      name: "efgh",
+      date: "2022-10-06T22:22:55",
+      waste_type: "Non-Recyclable",
+      msg: "kjrbjbkrjklbjbyrkg",
+      distance: 57.48,
+    },
+    {
+      name: "ijkl",
+      date: "2021-10-06T22:22:55",
+      waste_type: "Organic",
+      msg: "yjorhiotrjrhbijt",
+      distance: 48.45,
+    },
+    {
+      name: "mnop",
+      date: "2023-10-06T22:22:55",
+      waste_type: "Non-Recyclable",
+      msg: "jklnjtjlgnjltnbjlnjn",
+      distance: 55.47,
+    },
+    {
+      name: "qrst",
+      date: "2020-10-06T22:22:55",
+      waste_type: "Recyclable",
+      msg: "lnbklvnjrlkgnbvllfhngcblfgnhjlgbjdjbhvjdfhvjdfvjfjvjdfvhjfv",
+      distance: 66.48,
+    },
+  ]);
+  const today = new Date();
   return (
-    <div>
-      <table
-        class="table table-striped"
-        style={{ width: "92%", left: "6%", position: "absolute" }}
-      >
-        <thead>
-          <tr>
-            <th scope="col">S.No.</th>
-            <th scope="col">Username</th>
-            <th scope="col">Request</th>
-            <th scope="col">Waste Type</th>
-            <th scope="col">Status</th>
-            <th scope="col">Distance</th>
-          </tr>
-        </thead>
-
-        <tbody id="tableBody">
-          <tr>
-            <td>1</td>
-            <td>abcd</td>
-            <td>msgx1jjfj4rfhj4hfjf</td>
-            <td>Recyclable</td>
-            <td>
-              <div
-                style={{
-                  borderStyle: "solid",
-                  borderRadius: "15px",
-                  backgroundColor: "#e66e6e",
-                  maxWidth: "100px",
-                  textAlign: "center",
+    <div class="container-fluid row" style={{ marginLeft: "60px" }}>
+      {userDist.map((user, index) => (
+        <div
+          class="noteCard my-2 mx-2 card"
+          style={{ width: "20rem", backgroundColor: "white" }}
+        >
+          <div class="card-body">
+            {/* <h5 class="card-title" id="title ${index}"><b>${element.noteTitle}</b></h5> */}
+            <p class="card-text">Username:{user.name}</p>
+            <p class="card-text">Request Date:{user.date}</p>
+            <p class="card-text">Waste Type:{user.waste_type}</p>
+            <p class="card-text">Message:{user.msg}</p>
+            <p class="card-text">Distance:{user.distance}</p>
+            <hr />
+            <form>
+              <label htmlFor={index}>Enter Date and time:</label>
+              <input
+                type="datetime-local"
+                name="time"
+                min={user.date}
+                value={`${today.getYear()}-${today.getMonth()}-${today.getDate()}T${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`}
+              />
+              <button
+                onClick={(e) => {
+                  // console.log(
+                  //   `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}T${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+                  // );
+                  e.preventDefault();
+                  console.log("Request approved by collector");
+                  setUserDist(
+                    userDist.filter((item) => item.date != user.date)
+                  );
                 }}
+                class="btn btn-primary mx-2 my-3"
               >
-                Pending
-              </div>
-            </td>
-            <td>76.06</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>abcd</td>
-            <td>msgoewtghfjueonc</td>
-            <td>Recyclable</td>
-            <td>
-              <div
-                style={{
-                  borderStyle: "solid",
-                  borderRadius: "15px",
-                  backgroundColor: "#e66e6e",
-                  maxWidth: "100px",
-                  textAlign: "center",
-                }}
-              >
-                Pending
-              </div>
-            </td>
-            <td>74.04</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>abcd</td>
-            <td>lejghfuowewjfoeghfgvfhebgvvrfev</td>
-            <td>Non-Recyclable</td>
-            <td>
-              <div
-                style={{
-                  borderStyle: "solid",
-                  borderRadius: "15px",
-                  backgroundColor: "#6bde6b",
-                  maxWidth: "100px",
-                  textAlign: "center",
-                }}
-              >
-                Accepted
-              </div>
-            </td>
-            <td>80.98</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>abcd</td>
-            <td>klehbvcitwekjf4oehgfdijrd</td>
-            <td>Recyclable</td>
-            <td>
-              <div
-                style={{
-                  borderStyle: "solid",
-                  borderRadius: "15px",
-                  backgroundColor: "#e66e6e",
-                  maxWidth: "100px",
-                  textAlign: "center",
-                }}
-              >
-                Pending
-              </div>
-            </td>
-            <td>65.05</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>abcd</td>
-            <td>kleh;bvouhelfcxkjwkhfvcgefw</td>
-            <td>Recyclable</td>
-            <td>
-              <div
-                style={{
-                  borderStyle: "solid",
-                  borderRadius: "15px",
-                  backgroundColor: "#e66e6e",
-                  maxWidth: "100px",
-                  textAlign: "center",
-                }}
-              >
-                Pending
-              </div>
-            </td>
-            <td>45.56</td>
-          </tr>
-        </tbody>
-      </table>
+                Add to schedule
+              </button>
+            </form>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
