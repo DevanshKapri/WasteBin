85186c2dfd02b990bca44700c29826945963ccd9
Parag Jhunjhunwala <87004753+paragjjw@users.noreply.github.com>
2023-02-11 17:59:20 +0530

    Update index.js

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,35 +1,35 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import { Grid, Paper } from '@mui/material';
-import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
-import Grid_comp from './comp/Grid_comp';
-import Chart from './comp/Chart';
-import PieChart from './comp/PieChart';
-import Table from './comp/Table_comp';
-import Table_comp from './comp/Table_comp';
-import { DonorForm } from './comp/Form';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import UserRequests from './UserRequests';
-import axios from 'axios';
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { Grid, Paper } from "@mui/material";
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
+import Grid_comp from "./comp/Grid_comp";
+import Chart from "./comp/Chart";
+import PieChart from "./comp/PieChart";
+import Table from "./comp/Table_comp";
+import Table_comp from "./comp/Table_comp";
+import { DonorForm } from "./comp/Form";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import UserRequests from "./UserRequests";
+import axios from "axios";
 // import UserDistance from './UserDistance';
 const drawerWidth = 240;
 
@@ -103,32 +103,29 @@ const Dashboard = () => {
   const [requests, setRequests] = React.useState([]);
   const [user, setUser] = React.useState([]);
   const getRequests = async () => {
-    await axios.get('http://localhost:8000/getRequests')
-      .then(res => {
+    await axios
+      .get("http://localhost:8000/getRequests")
+      .then((res) => {
         // console.log(res.data);
         setRequests(res.data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+  };
 
   useEffect(() => {
-    const token = JSON.parse(localStorage.getItem('token'));
-    const User = JSON.parse(localStorage.getItem('user'));
+    const token = JSON.parse(localStorage.getItem("token"));
+    const User = JSON.parse(localStorage.getItem("user"));
     if (token && User) {
       setUser(User);
-      if(User.role === 'User')
-      {
+      if (User.role === "User") {
         getRequests();
       }
-    }
-    else
-      navigate('/');
-    if (User.role === 'collector' && User.status === 'unverified')
-      navigate('/');
-  }, [])
-
+    } else navigate("/");
+    if (User.role === "collector" && User.status === "unverified")
+      navigate("/");
+  }, []);
 
   const data = requests.filter((item) => item.user === user._id);
   console.log(data);
@@ -144,134 +141,129 @@ const Dashboard = () => {
   };
 
   return (
-    <>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar position="fixed" open={open}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Waste Bin
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: open ? "initial" : "center",
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
               sx={{
-                marginRight: 5,
-                ...(open && { display: "none" }),
+                minWidth: 0,
+                mr: open ? 3 : "auto",
+                justifyContent: "center",
               }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap component="div">
-              Waste Bin
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <DrawerHeader>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === "rtl" ? (
-                <ChevronRightIcon />
-              ) : (
-                <ChevronLeftIcon />
-              )}
-            </IconButton>
-          </DrawerHeader>
-          <Divider />
-          <List>
-            <ListItemButton
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="History" sx={{ opacity: open ? 1 : 0 }} />
+          </ListItemButton>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: open ? "initial" : "center",
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
               sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
+                minWidth: 0,
+                mr: open ? 3 : "auto",
+                justifyContent: "center",
               }}
             >
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-              >
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="History" sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-            <ListItemButton
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" sx={{ opacity: open ? 1 : 0 }} />
+          </ListItemButton>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: open ? "initial" : "center",
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
               sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
+                minWidth: 0,
+                mr: open ? 3 : "auto",
+                justifyContent: "center",
               }}
             >
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-              >
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Profile" sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-            >
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-              >
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Logout" sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-          </List>
-        </Drawer>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <DrawerHeader />
-          <Grid container spacing={8}>
-            <Grid_comp />
-            <Grid_comp />
-            <Grid_comp />
-            <Grid_comp />
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Logout" sx={{ opacity: open ? 1 : 0 }} />
+          </ListItemButton>
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Grid container spacing={8}>
+          <Grid_comp />
+          <Grid_comp />
+          <Grid_comp />
+          <Grid_comp />
 
-            {/* <Grid item xs={3}>
+          {/* <Grid item xs={3}>
             <Paper sx = {{height : '150px'}}>
               <Typography variant="h5" component="h3">Paper</Typography>
             </Paper>
           </Grid> */}
-          </Grid>
+        </Grid>
 
-          <div
-            style={{
-              display: "flex",
-              padding: "5px 20px",
-              gap: "20px",
-              marignTop: "8rem",
-            }}
-          >
-            <PieChart />
-            <Chart />
-          </div>
+        <div
+          style={{
+            display: "flex",
+            padding: "5px 20px",
+            gap: "20px",
+            marignTop: "8rem",
+          }}
+        >
+          <PieChart />
+          <Chart />
+        </div>
 
-        <div className="Form" style={{ marignTop: "10rem" }} >
+        <div className="Form" style={{ marignTop: "10rem" }}>
           <DonorForm />
           <div />
         </div>
-        <UserRequests data = {data}/>
-        
-
+        <UserRequests data={data} />
       </Box>
-
-
-    </Box >
+    </Box>
   );
 };
 
